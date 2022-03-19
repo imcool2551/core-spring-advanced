ef5fbaed2004c5e7f41a3d68ca7951870b555cfd
SangWook <imcool2551@gmail.com>
2022-03-19 20:36:35 +0900

    resolve concurrency issue with threadlocal

---
+++ advanced/src/test/java/hello/advanced/trace/threadlocal/ThreadLocalServiceTest.java
@@ -0,0 +1,37 @@
+package hello.advanced.trace.threadlocal;
+
+import hello.advanced.trace.threadlocal.code.ThreadLocalService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ThreadLocalServiceTest {
+
+    private ThreadLocalService service = new ThreadLocalService();
+
+    @Test
+    void fieldService() {
+        log.info("main start");
+        Runnable userA = () -> service.logic("userA");
+        Runnable userB = () -> service.logic("userB");
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("thread-A");
+        Thread threadB = new Thread(userB);
+        threadB.setName("thread-B");
+
+        threadA.start();
+        sleep(100); // ThreadLocal 을 사용하기 때문에 동시성 문제 발생X
+        threadB.start();
+        sleep(3000); // 메인 쓰레드 종료 대기
+        log.info("main exit");
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
