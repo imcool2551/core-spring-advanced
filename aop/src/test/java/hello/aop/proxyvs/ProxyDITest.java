64218c5096445b01fe188c43e929bc7222da0199
SangWook <imcool2551@gmail.com>
2022-04-01 18:41:21 +0900

    proxy corner cases

---
+++ aop/src/test/java/hello/aop/proxyvs/ProxyDITest.java
@@ -0,0 +1,34 @@
+package hello.aop.proxyvs;
+
+import hello.aop.member.MemberService;
+import hello.aop.member.MemberServiceImpl;
+import hello.aop.proxyvs.code.ProxyDIAspect;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+/**
+ * JDK proxy 는 구체 클래스 기반으로 의존관계 주입이 불가능하다.
+ */
+@Slf4j
+//@SpringBootTest(properties = "spring.aop.proxy-target-class=false") // JDK proxy
+@SpringBootTest(properties = "spring.aop.proxy-target-class=true") // CGLIB proxy
+@Import(ProxyDIAspect.class)
+public class ProxyDITest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    MemberServiceImpl memberServiceImpl;
+
+    @Test
+    void go() {
+        log.info("memberService class={}", memberService.getClass());
+        log.info("memberServiceImpl class={}", memberServiceImpl.getClass());
+
+        memberServiceImpl.hello("hello");
+    }
+}
